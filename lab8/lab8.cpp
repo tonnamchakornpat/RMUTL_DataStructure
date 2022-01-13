f040151a0bbba812adc08c66a4a782ae6093f36f
Chakornpat <chakornpat0206@outlook.com>
2022-01-13 12:58:47 +0700

    edit lab 8

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -3,20 +3,21 @@
 
 int Number;
 float Number2, ans;
-int Power(float N, int M)
+
+int Pow(float N, int M)
 {
     int x;
     float y;
     if (M == 0)
     {
-        printf(".............Roll back point\n");
+        printf("-----------------Roll back point\n");
         return 1; // Base Criteria
     }
     else
     {
         x = M - 1;
-        printf("%2.2f^%d = %2.2f * %2.2f ^ %d\n", N, M, N, N, x); // todo Display before Recursive
-        y = Power(N , x );
+        printf("%2.2f^%d = %2.2f * %2.2f^%d\n", N, M, N, N, x); // todo Display before Recursive
+        y = Pow(N, x);
         printf("%2.2f^%d = %2.2f * %2.2f = %2.2f \n", N, M, N, y, N * y); // todo Dispaly AfterRecursive
         return N * y;
     }
@@ -27,17 +28,16 @@ int main()
     printf("RECURSIVE (Power) PROGRAM\n");
     printf("=============================\n");
     // N=0 ;
-    while (Number != -999 || Number2 != -999)
+    while (Number != -999 or Number2 != -999)
     {
         printf("Enter Number ( -999 is END) = ");
         scanf(" %f %d", &Number2, &Number);
         if (Number >= 0 && Number2 >= 0)
         {
-            printf("N != N(N-1)! \n");
             printf("-----------------Roll forward point\n");
-            ans = Power(Number2, Number);
+            ans = Pow(Number2, Number);
             printf("\n%.2f^%d = %.2f \n", Number2, Number, ans);
-            printf("-----------------Finished\n");
+            printf("---------------------------Finished\n");
             if (getch() == 'e')
                 break;
         }
