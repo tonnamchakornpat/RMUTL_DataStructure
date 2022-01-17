8e3a99fe4db80d723ff54e029773a441a2b40ccd
Chakornpat <chakornpat0206@outlook.com>
2022-01-17 15:37:56 +0700

    add comment lab 8

--- lab9/Link_list.cpp
+++ lab9/Link_list.cpp
@@ -66,30 +66,30 @@ Node *Allocate() //* Allocate 1 node from storage  pool
 void CreateNNode(int n)
 {
     int i, temp;
-    H = p;
-    H1 = p;
+    H = p;  //? set H -> p
+    H1 = p; //? H1 -> p
     for (i = 1; i <= n; i++)
     {
         p = Allocate();
         temp = 1 + rand() % 99;
         p->info = temp;
-        H1->link = p;
-        H1 = p;
-        H1->link = H;
+        H1->link = p; //? Link H1 -> p
+        H1 = p;       //? H1 -> p
+        H1->link = H; //? Link H1 -> H
     }
 }
 
 void ShowAllNode()
 {
     printf("H = %x \n", H);
-    p = H->link;
+    p = H->link; //? p -> Link H
     i = 1;
-    while (p->info != HeadData)
+    while (p->info != HeadData) //? Check back to H node?
     {
         printf("%d) %x \t", i, p);
         printf("INFO : %d \t", p->info);
         printf("LINK : %x\n", p->link);
-        p = p->link;
+        p = p->link; // todo set p to next node
         i++;
     }
 }
@@ -97,23 +97,23 @@ void ShowAllNode()
 void InsertAfter(int data1)
 {
     int temp;
-    if (H->link == H)
+    if (H->link == H) // ? check have node ?
         printf("Circular Linked List have no node!!....\n");
     else
     {
-        H1 = H->link;
-        while (H1->info != HeadData)
+        H1 = H->link;                //? H1 -> Link H
+        while (H1->info != HeadData) //? Check back to H node ?
         {
-            if (H1->info == data1)
+            if (H1->info == data1) //? check data1 == info ?
             {
                 p = Allocate();
                 printf("\nInsert data :");
                 scanf("%d", &temp);
-                p->info = temp;
-                p->link = H1->link;
-                H1->link = p;
+                p->info = temp;     //? info p = temp
+                p->link = H1->link; //? link p -> link H1
+                H1->link = p;       //? link H1 -> p
             }
-            H1 = H1->link;
+            H1 = H1->link; // todo set H1 to next node
         }
     }
 }
@@ -121,28 +121,28 @@ void InsertAfter(int data1)
 void DeleteAfter(int data1)
 {
     int temp;
-    if (H->link == H)
+    if (H->link == H) //? check have node ?
         printf("Circular Linked List have no node!!....\n");
     else
     {
-        H1 = H->link;
-        while (H1->info != HeadData)
+        H1 = H->link;                //? H1 -> Link H
+        while (H1->info != HeadData) //? Check back to H node ?
         {
-            if (H1->info == data1)
+            if (H1->info == data1) //? check data1 == info ?
             {
-                if (H1->link == H)
+                if (H1->link == H) //? check node is H ?
                     printf("This is the Head");
                 else
                 {
-                    p = H1->link;
-                    if (p->link == H)
-                        H1->link = H;
+                    p = H1->link;     //? p -> next node (Link H1 )
+                    if (p->link == H) //?  check p is last node ?
+                        H1->link = H; //? set Link H1 -> H
                     else
-                        H1->link = p->link;
-                    free(p);
+                        H1->link = p->link; //? Link H1 -> Link p
+                    free(p);                //? free node to memory
                 }
             }
-            H1 = H1->link;
+            H1 = H1->link; // todo set H1 to next node
         }
     }
 }
